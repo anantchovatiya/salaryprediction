62fd816a0f27d4f10a89b7aa9dd219d2b5e58442
anantchovatiya <147299670+anantchovatiya@users.noreply.github.com>
2025-03-02 00:46:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 import pickle
 import numpy as np
+from flask_cors import CORS
+
+
+model = pickle.load(open('salary_model.pkl', 'rb'))
+mlb = pickle.load(open('mlb.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
+@app.route('/')
+def home():
+    return """
+    <h1>Welcome to the Salary Prediction API</h1>
+    <p>Use the /predict endpoint to get salary predictions based on skills and experience.</p>
+    """
 
-@app.after_request
-def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-    return response
-
-
-model = pickle.load(open("salary_model.pkl", "rb"))
-mlb = pickle.load(open("mlb.pkl", "rb"))
-scaler = pickle.load(open("scaler.pkl", "rb"))
-
-@app.route("/predict", methods=["POST"])
+@app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-    skills = data.get("skills", [])
-    experience = data.get("experience", 0)
+    skills = data.get('skills', [])
+    experience = data.get('experience', 0)
 
+    # Transform input skills
     input_skills = mlb.transform([skills])
     experience_scaled = scaler.transform([[experience]])
     input_data = np.hstack((input_skills, experience_scaled))
 
+    # Predict salary
     predicted_salary = model.predict(input_data)[0]
-    return jsonify({"predicted_salary": round(predicted_salary, 2)})
 
-if __name__ == "__main__":
+    return jsonify({'predicted_salary': round(predicted_salary, 2)})
+
+if __name__ == '__main__':
     app.run(debug=True)
